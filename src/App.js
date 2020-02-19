5a8c7b8da6f7366ca13abd65886dc518fac9af48
Radhika Murugesan <radhika.murugesan@perficient.com>
2020-02-19 16:59:32 +0530

    Readme Updated

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ContactForm from './components/contactForm/ContactForm';
-import ContactList from './components/contactList/ContactList';
+import React, { Component } from "react";
+import "./App.css";
+import ContactForm from "./components/contactForm/ContactForm";
+import ContactList from "./components/contactList/ContactList";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      name: '',
-      age: '',
-      phone: '',
+      name: "",
+      age: "",
+      phone: "",
       items: []
-    }
-  };
+    };
+  }
 
-  handleInputChange = (e) => {
+  handleInputChange = e => {
     let input = e.target;
     let name = e.target.name;
     let value = input.value;
 
     this.setState({
       [name]: value
-    })
+    });
   };
 
-  addContact = (e) => {
+  addContact = e => {
     e.preventDefault();
-
     let items = [...this.state.items];
 
     items.unshift({
@@ -39,50 +37,42 @@ class App extends Component {
 
     this.setState({
       items,
-     name:'',
-     age: '',
-     phone: ''
+      name: "",
+      age: "",
+      phone: ""
     });
-  }
+  };
   onReset = () => {
-    this.setState({      
-     name:'',
-     age: '',
-     phone: ''
+    this.setState({
+      name: "",
+      age: "",
+      phone: ""
     });
-  }
+  };
 
-  onDelete = (key) => {
-    console.log('pkey',key);
-    
-    var array = [this.state.items]; 
+  onDelete = key => {
+    var array = [...this.state.items];
     array.splice(key, 1);
-    this.setState({items: array});
-  
-  }
-  render(){
-    
-    
+    this.setState({ items: array });
+  };
+
+  render() {
     return (
       <div className="App">
-       <ContactForm 
-       name = {this.state.name}
-       age = {this.state.age}
-       phone = {this.state.phone}
-       handleInputChange = {this.handleInputChange}
-       addContact = {this.addContact}
-       onReset = {this.onReset}
-       />
-       {this.state.items ?
-       <ContactList
-       items = {this.state.items}
-       onDelete = {this.onDelete}
-      /> :
-       null
-  }
+        <ContactForm
+          name={this.state.name}
+          age={this.state.age}
+          phone={this.state.phone}
+          handleInputChange={this.handleInputChange}
+          addContact={this.addContact}
+          onReset={this.onReset}
+        />
+        {this.state.items.length > 0 ? (
+          <ContactList items={this.state.items} onDelete={this.onDelete} />
+        ) : null}
       </div>
     );
   }
-  }
+}
 
 export default App;
--- src/components/contactList/ContactList.js
+++ src/components/contactList/ContactList.js
@@ -1,43 +1,31 @@
-import React, { Component } from 'react';
-import './styles.css';
-export default function ContactList (props) {
-    
-    onClickRow = () => {
-        return(
-
-        )
-    }
-   
-        const items = props.items;
-        console.log('props', items);
-        return(
-            <div className="container">
-             
-            <table>
-              <tbody>
-                <tr>
-                  <th>Name</th>
-                  <th>Age</th>
-                  <th>Phone</th>
-                  <th>Actions</th>
-                </tr>
-                {items.map((item,key) => {
-                    console.log('key', key);
-                    
-                  return (
-                    <tr>
-                      <td>{item.name}</td>
-                      <td>{item.age}</td>
-                      <td>{item.phone}</td>
-                      <td><button onClick={props.onDelete(key)}>Delete</button></td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        );
-           
-  
-    
+import React, { Component } from "react";
+import "./styles.css";
+export default function ContactList(props) {
+  const items = props.items;
+  return (
+    <div className="container">
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+          {items.map((item, key) => {
+            return (
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.phone}</td>
+                <td>
+                  <button className="btn" onClick={() => props.onDelete(key)}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
